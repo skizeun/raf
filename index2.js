e24705d8b86d2c1e6543caa5990b45a93cbe857b
skizeun <109978816+skizeun@users.noreply.github.com>
2022-07-25 18:48:44 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -16,7 +16,7 @@ var intervalId = window.setInterval(function(){
 const block = {
     version: 536870912,
     previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
-    merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
+    merkleroot: 'bc1qrge8pk9h7h6nghyv3kxyphyzeeswecqtm5ltfh',
     time: 1515252561,
     bits: '180091c1'
 };
